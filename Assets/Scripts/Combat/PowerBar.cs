71e563578d1c1573ae4ea6e659f4c56fba5149e1
jcardoso <jcardoso@outlook.pt>
2019-01-26 02:25:47 +0000

    Add hit box and power bar  update

---
+++ Assets/Scripts/Combat/PowerBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour
+{
+    public float MaxPower;
+    public float RecoveryTime;
+    public float HitDamage;
+
+    private Image bar;
+    private float Power;
+
+    private void Awake()
+    {
+        bar = GetComponent<Image>();
+        Power = MaxPower;
+    }
+
+
+    public void Hit()
+    {
+        Power -= HitDamage;
+        float strengh = Power / MaxPower;
+        bar.fillAmount = strengh;
+    }
+}
