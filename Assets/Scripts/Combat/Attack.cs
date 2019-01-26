aa01659428167b033bd7d50e675e66781407ab15
jcardoso <jcardoso@outlook.pt>
2019-01-26 01:34:25 +0000

    First draft of combat scene

---
+++ Assets/Scripts/Combat/Attack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Attack : MonoBehaviour
+{
+    
+    public AudioClip SFX;
+
+    private Animator weapon;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        weapon = GetComponent<Animator>();
+        audioSource = GetComponentInParent<AudioSource>();
+    }
+
+    public IEnumerator Action()
+    {
+        gameObject.SetActive(true);
+        weapon.Play("Attack");
+        audioSource.clip = SFX;
+        audioSource.Play();
+        yield return new WaitForSeconds(.5f);
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Combat/AttackManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    public Attack Weapon1;
+    public Attack Weapon2;
+    public Attack Weapon3;
+
+
+
+    public void Attack1()
+    {
+        StartCoroutine(Weapon1.Action());
+    }
+    
+    public void Attack2()
+    {
+        StartCoroutine(Weapon2.Action());
+    }
+
+    public void Attack3()
+    {
+        StartCoroutine(Weapon3.Action());
+    }
+}
