83e94cbc29e17d95a11f7637ddf079dedf974c66
Nuno Xu <nuno.xu@gmail.com>
2019-01-26 05:36:28 +0000

    Progress

---
+++ Assets/Scripts/UI/Pause/ToMainMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI.Pause
+{
+    public class ToMainMenuButton : MonoBehaviour
+    {
+        public string MainMenuScene;
+
+        public void ToMainMenu()
+        {
+            Time.timeScale = 1f;
+            GameManager.Instance.OpenScene(MainMenuScene);
+            GameManager.Instance.ResetGame();
+        }
+    }
+}
